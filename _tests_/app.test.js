e2cedd75192dd645371e556054f89bbf9fcd3dad
mide358 <iyimide.adegunloye@gmail.com>
2022-04-26 15:20:42 +0100

    added test for actions

--- _tests_/app.test.js
+++ _tests_/app.test.js
@@ -21,13 +21,63 @@ describe('test', () => {
     app = new App();
   });
 
-  it('test the bot', async () => {
+  it('test the welcome message bot', async () => {
     await app.start();
     expect(app.message).toBeCalledWith('hello', expect.any(Function));
   });
 
-  it('test the bot', async () => {
+  it('test hey|hi response', async () => {
     await app.start();
-    expect(app.message).toBeCalledWith(/hey/i, expect.any(Function));
+    expect(app.message).toBeCalledWith(/hey|hi/i, expect.any(Function));
+  });
+  it('test the team_join event', async () => {
+    await app.start();
+    expect(app.event).toBeCalledWith('team_join', expect.any(Function));
+  });
+
+  it('test the develop metrics action', async () => {
+    await app.start();
+    expect(app.action).toBeCalledWith('develop', expect.any(Function));
+  });
+  it('test the join a meeting action', async () => {
+    await app.start();
+    expect(app.action).toBeCalledWith('joinMeet', expect.any(Function));
+  });
+  it('test the contribute to code action', async () => {
+    await app.start();
+    expect(app.action).toBeCalledWith('contribute', expect.any(Function));
+  });
+  it('test the helpWithWebsite action', async () => {
+    await app.start();
+    expect(app.action).toBeCalledWith('helpWithWebsite', expect.any(Function));
+  });
+  it('test the documentation action', async () => {
+    await app.start();
+    expect(app.action).toBeCalledWith('docs', expect.any(Function));
+  });
+  it('test the mentorship action', async () => {
+    await app.start();
+    expect(app.action).toBeCalledWith('mentorship', expect.any(Function));
+  });
+  it('test the mentorship action', async () => {
+    await app.start();
+    expect(app.action).toBeCalledWith(
+      'mentorship_selection',
+      expect.any(Function)
+    );
+  });
+  it('test the implement_metrics action', async () => {
+    await app.start();
+    expect(app.action).toBeCalledWith(
+      'implement_metrics',
+      expect.any(Function)
+    );
+  });
+  it('test the learn_something_else action', async () => {
+    await app.start();
+    expect(app.action).toBeCalledWith(
+      'learn_something_else',
+      expect.any(Function)
+    );
   });
 });
